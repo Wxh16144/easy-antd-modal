1cc707bd5396e7793aaa28c0400a1b55c6fc645b
wuxh <wxh1220@gmail.com>
2023-07-19 14:11:11 +0800

    chore: update release config

--- .releaserc.js
+++ .releaserc.js
@@ -11,5 +11,18 @@ module.exports = {
         changelogFile: 'CHANGELOG.md',
       },
     ],
+    [
+      '@semantic-release/github',
+      {
+        message: "chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}'",
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+      },
+    ],
+    '@semantic-release/npm',
   ],
 };

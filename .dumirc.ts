dec90199fd2f265e33ae5b588d2311494e554f6d
wuxh <wxh1220@gmail.com>
2023-07-19 12:59:00 +0800

    ci: add workflows

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'dumi';
 import { homepage } from './package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+// 不是预览模式 同时是生产环境
+const isProdSite = process.env.PREVIEW !== '1' && isProd;
+
+const name = 'easy-antd-modal';
+
 export default defineConfig({
   themeConfig: {
-    name: 'easy-antd-modal',
+    name,
     github: homepage,
   },
+  base: isProdSite ? `/${name}/` : '/',
+  publicPath: isProdSite ? `/${name}/` : '/',
   html2sketch: {},
   mfsu: false,
 });

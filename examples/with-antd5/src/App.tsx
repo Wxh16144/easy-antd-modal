06fa06316dbfe31dbff126250b9b7ab99e7bca8d
wuxh <wxh1220@gmail.com>
2023-08-01 11:29:36 +0800

    chore: update demo

--- examples/with-antd5/src/App.tsx
+++ examples/with-antd5/src/App.tsx
@@ -1,8 +1,9 @@
-import { Button } from 'antd';
+import { Button, Typography, version } from 'antd';
 import Modal from 'easy-antd-modal';
 
 export default () => (
   <>
+    <Typography.Title level={2}>antd version: {version}</Typography.Title>
     <Modal title="easy-antd-modal" trigger={<Button type="primary">Click Me</Button>}>
       I ❤️ antd
     </Modal>

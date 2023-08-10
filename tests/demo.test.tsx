5a1aa61c6eb43342bc1ae7ddfddf0a1865ebcc6f
wuxh <wxh1220@gmail.com>
2023-08-10 11:22:31 +0800

    test: add demos test

---
+++ tests/demo.test.tsx
@@ -0,0 +1,41 @@
+import { render } from '@testing-library/react';
+import { EasyAntdModalProvider } from 'easy-antd-modal';
+import fg from 'fast-glob';
+import * as path from 'path';
+import { act } from 'react-dom/test-utils';
+import { vi } from 'vitest';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  // @ts-ignore
+  global.fetch = vi.fn(() =>
+    Promise.resolve({ json: () => Promise.resolve({}), text: () => Promise.resolve('') }),
+  );
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
+const baseDir = path.join(__dirname, '..');
+const examplePath = path.join(baseDir, 'docs/examples');
+
+// 支持 examples 下的所有非_开头的tsx文件
+const allExamples = fg.globSync('**/[!_]*.tsx', {
+  cwd: examplePath,
+});
+
+allExamples.forEach((file) => {
+  it(`renders ${file} correctly`, async () => {
+    const Demo = await import(path.join(examplePath, file));
+
+    // console.log(`测试组件${dir} DEMO:${demoName}`);
+    const wrapper = render(<Demo.default />, { wrapper: EasyAntdModalProvider });
+    act(() => {
+      vi.runAllTimers();
+    });
+
+    expect(wrapper.container).toMatchSnapshot();
+    wrapper.unmount();
+  });
+});
--- tests/test-setup.ts
+++ tests/test-setup.ts
@@ -1,1 +1,20 @@
 import '@testing-library/jest-dom';
+
+/* eslint-disable global-require */
+if (typeof window !== 'undefined') {
+  /**
+   * ref: https://github.com/vitest-dev/vitest/issues/821#issuecomment-1046954558
+   * ref: https://github.com/ant-design/ant-design/issues/18774
+   */
+  if (!window.matchMedia) {
+    Object.defineProperty(global.window, 'matchMedia', {
+      value: vi.fn((query) => ({
+        matches: query.includes('max-width'),
+        addListener: () => {},
+        addEventListener: () => {},
+        removeListener: () => {},
+        removeEventListener: () => {},
+      })),
+    });
+  }
+}

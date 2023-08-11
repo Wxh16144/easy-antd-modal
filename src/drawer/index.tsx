ba8692766f132fb849cf174850495f24eb69cee2
wuxh <wxh1220@gmail.com>
2023-08-11 18:51:50 +0800

    feat: drawer 支持 antd CP prefixCls

--- src/drawer/index.tsx
+++ src/drawer/index.tsx
@@ -1,10 +1,14 @@
 import type { DrawerProps as AntdDrawerProps } from 'antd';
-import { Drawer as AntdDrawer } from 'antd';
+import { ConfigProvider as AntdConfigProvider, Drawer as AntdDrawer } from 'antd';
+import * as React from 'react';
 import { UseModalEnhancedProps, useModalEnhanced } from '../hooks';
 
 export type DrawerProps = Omit<AntdDrawerProps, 'visible'> & UseModalEnhancedProps;
 
 const Modal = (props: DrawerProps) => {
+  const { getPrefixCls } = React.useContext(AntdConfigProvider.ConfigContext);
+  const drawerCls = props.prefixCls ? `${props.prefixCls}` : `easy-${getPrefixCls()}-drawer`;
+
   const [visible, { close }, { trigger, content }, restProps] = useModalEnhanced(props);
 
   const handleModalCancel: DrawerProps['onClose'] = (event) => {
@@ -15,7 +19,7 @@ const Modal = (props: DrawerProps) => {
   return (
     <>
       {trigger}
-      <AntdDrawer open={visible} {...restProps} onClose={handleModalCancel}>
+      <AntdDrawer open={visible} {...restProps} onClose={handleModalCancel} prefixCls={drawerCls}>
         {content}
       </AntdDrawer>
     </>

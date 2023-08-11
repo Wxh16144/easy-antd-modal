724f6bca8a3dde5fb49ffd64ee4d0c0359aee9a1
wuxh <wxh1220@gmail.com>
2023-08-11 18:31:56 +0800

    feat: 添加 easy-antd-modal cls 支持 antd CP 组件修改

--- src/drag-modal/BaseModal.tsx
+++ src/drag-modal/BaseModal.tsx
@@ -1,5 +1,7 @@
 import { useDraggable } from '@dnd-kit/core';
 import { CSS } from '@dnd-kit/utilities';
+import { ConfigProvider as AntdConfigProvider } from 'antd';
+import * as React from 'react';
 import type { DragModalProps } from '.';
 import Modal from '../modal';
 
@@ -9,7 +11,20 @@ export interface BaseModalProps extends DragModalProps {
 }
 
 function BaseModal(props: BaseModalProps) {
-  const { modalRender, title, offsetX, offsetY, ...resetProps } = props;
+  const {
+    modalRender,
+    title,
+    offsetX,
+    offsetY,
+    prefixCls: customizePrefixCls,
+    ...resetProps
+  } = props;
+
+  const { getPrefixCls } = React.useContext(AntdConfigProvider.ConfigContext);
+  const dragModalCls = customizePrefixCls
+    ? `${customizePrefixCls}`
+    : `easy-${getPrefixCls()}-drag-modal`;
+
   const { attributes, isDragging, listeners, setNodeRef, transform } = useDraggable({
     id: 'easy-antd-modal-draggable-modal',
   });
@@ -23,15 +38,25 @@ function BaseModal(props: BaseModalProps) {
         top: offsetY,
         left: offsetX,
       }}
+      className={`${dragModalCls}-content__wrapper`}
     >
       {modalRender?.(rawNode) ?? rawNode}
     </div>
   );
 
+  // Compliance with BEM norms
+  const titleCls = [
+    `${dragModalCls}-title__inner`,
+    isDragging && `${dragModalCls}-title__inner_dragging`,
+  ]
+    .filter(Boolean)
+    .join(' ');
+
   return (
     <Modal
+      prefixCls={dragModalCls}
       title={
-        <div {...listeners} {...attributes} style={{ cursor: 'move' }}>
+        <div {...listeners} {...attributes} style={{ cursor: 'move' }} className={titleCls}>
           {title}
         </div>
       }
--- src/modal/index.tsx
+++ src/modal/index.tsx
@@ -1,10 +1,14 @@
 import type { ModalProps as AntdModalProps } from 'antd';
-import { Modal as AntdModal } from 'antd';
+import { ConfigProvider as AntdConfigProvider, Modal as AntdModal } from 'antd';
+import * as React from 'react';
 import { UseModalEnhancedProps, useModalEnhanced } from '../hooks';
 
 export type ModalProps = Omit<AntdModalProps, 'visible'> & UseModalEnhancedProps;
 
 const Modal = (props: ModalProps) => {
+  const { getPrefixCls } = React.useContext(AntdConfigProvider.ConfigContext);
+  const modalCls = props.prefixCls ? `${props.prefixCls}` : `easy-${getPrefixCls()}-modal`;
+
   const [visible, { close }, { trigger, content }, restProps] = useModalEnhanced(props);
 
   const handleModalOk: ModalProps['onOk'] = (event) => {
@@ -20,7 +24,13 @@ const Modal = (props: ModalProps) => {
   return (
     <>
       {trigger}
-      <AntdModal open={visible} {...restProps} onOk={handleModalOk} onCancel={handleModalCancel}>
+      <AntdModal
+        open={visible}
+        {...restProps}
+        prefixCls={modalCls}
+        onOk={handleModalOk}
+        onCancel={handleModalCancel}
+      >
         {content}
       </AntdModal>
     </>

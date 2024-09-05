46a3252c1bd889733260a9b7094a2dcfebaab204
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-09-05 11:37:48 +0800

    chore: 改进 TS 描述

--- src/drawer/index.tsx
+++ src/drawer/index.tsx
@@ -9,7 +9,30 @@ import type { AnyObj } from '../types';
 /** @internal */
 type CloseCallback = Pick<AntdDrawerProps, 'onClose'>;
 
-export type DrawerProps = Omit<AntdDrawerProps, 'visible' | 'children'> & UseModalEnhancedProps;
+/**
+ * @description 方便用户自定义 `Modal` 的 `props`
+ * @see [easy-antd-modal/typescript](https://wxh16144.github.io/easy-antd-modal/typescript)
+ * @example
+ * ```tsx
+ * // 这段可以直接添加到你的任何 `.ts` 文件中，例如 `antd-modal.ts`
+ * // 也可以添加到一个 `.d.ts` 文件中。确保这个文件包含在项目的 `tsconfig.json` 中的 "file" 字段内。
+ * import 'easy-antd-modal'
+ *
+ * declare module 'easy-antd-modal' {
+ *  interface DrawerProps {
+ *    // `antd` 的 `Modal` 组件的 `visible` 属性
+ *    visible?: boolean
+ *  }
+ * }
+ *
+ * // 为了确保这个文件被当作一个模块，添加至少一个 `export` 声明
+ * export {}
+ * ```
+ */
+export interface DrawerProps
+  extends Omit<AntdDrawerProps, 'visible' | 'children' | 'onClick'>,
+    UseModalEnhancedProps {}
+
 /**
  * @description 方便用户自定义 `Modal` 的 `props`
  * @since 1.6.0
